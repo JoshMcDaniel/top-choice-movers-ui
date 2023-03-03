74681509ccc105904a6a2b3b5100dc4d76eb1b11
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2023-03-03 09:24:33 -0500

    Fix Services font size

--- Components/Services/ServiceSummarySection.tsx
+++ Components/Services/ServiceSummarySection.tsx
@@ -8,7 +8,9 @@ type Props = {
 const ServiceSummarySection = (props: Props) => {
   return (
     <section className="grid grid-flow-row gap-8 w-full bg-white px-4 p-12 lg:px-12 mb-12 items-start lg:justify-center">
-      <h3 className="text-2xl lg:text-4xl font-extrabold">Moving Services</h3>
+      <h3 className="text-2xl lg:text-4xl font-extrabold uppercase">
+        Moving Services
+      </h3>
       <div className="m-auto max-w-5xl">
         <ServicesGroupSummary services={props.services} />
       </div>

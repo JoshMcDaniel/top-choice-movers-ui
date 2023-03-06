28403debe776a7f9dedfacbabd26955a20142454
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2023-03-05 23:08:30 -0500

    Hard coding divider color to fix prod mode bug

--- Components/Dividers/BlurryHorizontalDivider.tsx
+++ Components/Dividers/BlurryHorizontalDivider.tsx
@@ -1,12 +1,17 @@
 type Props = {
-  color: string;
+  color?: string;
   height?: string;
 };
 
 const BlurryHorizontalDivider = (props: Props) => {
-  const color = `bg-transparent bg-gradient-to-r from-transparent via-${props.color} to-transparent`;
   const height = props.height ? `h-${props.height}` : 'h-0.5';
-  return <hr className={`${height} border-t-0 ${color}`} />;
+  // Hard coding "secondary" color for now.
+  // Doesn't work with dynamic values in prod mode.
+  return (
+    <hr
+      className={`${height} border-t-0 bg-transparent bg-gradient-to-r from-transparent via-secondary to-transparent`}
+    />
+  );
 };
 
 export default BlurryHorizontalDivider;
